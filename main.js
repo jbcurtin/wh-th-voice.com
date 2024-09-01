ae3b4efe061ee60c7e81ea542fd369cc7b555ad8
Joseph Curtin <jbcurtincode@gmail.com>
2024-09-01 14:07:01 +0900

    Initial publish

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+  $('table').addClass('table');
+  $('table').addClass('is-bordered')
+  $('table').addClass('is-fullwidth');
+  $('table').addClass('has-text-centered')
+})
